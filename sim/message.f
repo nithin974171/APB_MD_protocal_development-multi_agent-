aedc19a1dcb1fd3a11c3aeabf9358a52f9569e0e
nithin D S <1ms20ec404@gmail.com>
2026-03-25 15:20:26 +0530

    Update verbosity settings in message.f

--- sim/message.f
+++ sim/message.f
@@ -1,1 +1,6 @@
 
++uvm_set_verbosity=uvm_test_top*,_ALL_,UVM_NONE,time,0
++uvm_set_verbosity=uvm_test_top*,RX_FIFO,UVM_MEDIUM,time,0
+
+//+uvm_set_verbosity=*,REG_PREDICT,UVM_HIGH,time,0
++uvm_set_verbosity=*md_*_agent.monitor*,ITEM_END,UVM_LOW,time,9
